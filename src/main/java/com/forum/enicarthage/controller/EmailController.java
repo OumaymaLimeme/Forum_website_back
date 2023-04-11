4c484a397054e41bf34bea96b562e4b3670ad3bd
Oumayma02 <oumayma2000limeme@gmail.com>
2023-04-11 01:51:47 +0100

    backend_mail_sent

--- src/main/java/com/forum/enicarthage/controller/EmailController.java
+++ src/main/java/com/forum/enicarthage/controller/EmailController.java
@@ -3,10 +3,11 @@ package com.forum.enicarthage.controller;
 import com.forum.enicarthage.model.Email;
 import com.forum.enicarthage.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 public class EmailController {
     @Autowired
